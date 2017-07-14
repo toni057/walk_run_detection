19de9a50cce0d00ce4875422bae3a690e0eef76d
tb <toni.blaslov@gmail.com>
2017-07-14 23:48:30 +0100

    added glmnet

--- walk_run_detection.R
+++ walk_run_detection.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(caret)
 library(gbm)
 library(ranger)
+library(glmnet)
 
 library(ROCR)
 library(FactoMineR)
@@ -43,21 +44,29 @@ d2 <- d1 %>%
    cbind.data.frame(d, .) %T>%
    head()
 
-
-fmla <- activity ~ acceleration_x + acceleration_y + acceleration_z + gyro_x + gyro_y + gyro_z + x + y + z
-
-m <- glm(fmla, d2, family = binomial())
-fitted <- m$fitted.values
-
-
-pca <- PCA(d2[,5:13], graph = F, ncp = 9)
-fviz_pca(pca)
-fviz_eig(pca)
-
-
+# split to train and test
 trainind <- createDataPartition(d2$activity, p = .8, list = F, times = 1)
 train <- d2[trainind,]
 test <- d2[-trainind,]
+
+
+# formula
+fmla <- activity ~ acceleration_x + acceleration_y + acceleration_z + gyro_x + gyro_y + gyro_z + x + y + z
+
+
+m <- glm(fmla, train, family = binomial())
+
+
+m <- glmnet(model.matrix(fmla, d2), d2$activity, family="binomial", lambda = seq(1e-4, 1e-3, length.out = 100), )
+trfit <- predict(m, model.matrix(fmla, train))
+tefit <- predict(m, model.matrix(fmla, test))
+
+auc <- apply(trfit, 2, get.AUC, lab=train$activity)
+plot(m$lambda, auc)
+
+auc <- apply(tefit, 2, get.AUC, lab=test$activity)
+plot(m$lambda, auc)
+
 
 m <- gbm(fmla, distribution = 'bernoulli', data = train, n.trees = 100, shrinkage = 0.1, interaction.depth = 5, n.cores = 4, verbose = T)
 trfit <- m$fit
@@ -66,25 +75,6 @@ tefit <- predict(m, test, n.trees = m$n.trees)
 m <- ranger(fmla, train, num.trees = 1000, num.threads = 4, verbose = T, min.node.size = 5)
 trfit <- m$predictions
 tefit <- predict(m, test)$predictions
-
-
-pred_tr <- prediction(trfit, train$activity)
-perf <- performance(pred_tr,"tpr","fpr")
-plot(perf)
-
-pred_te <- prediction(tefit, test$activity)
-perf <- performance(pred_te,"tpr","fpr")
-plot(perf)
-
-auc_tr <- performance(pred_tr,"auc")
-auc_te <- performance(pred_te,"auc")
-
-perf1 <- performance(pred_tr, "prec", "rec")
-plot(perf1)
-perf1 <- performance(pred_te, "prec", "rec")
-plot(perf1)
-
-
 
 
 get.ROC.df <- function(fit, lab, set) {
@@ -98,12 +88,17 @@ get.ROC.df <- function(fit, lab, set) {
               set = set)
 }
 
+get.AUC <- function(fit, lab, set) {
+   pred <- prediction(fit, lab)
+   perf <- performance(pred, "auc")
+   perf@y.values[[1]]
+}
+
 rbind.data.frame(get.ROC.df(trfit, train$activity, 'train'),
                  get.ROC.df(tefit, test$activity, 'test')) %>%
    ggplot(data = ., mapping = aes(x = x, y = y, color = set, group = set)) +
    geom_line() + 
-   coord_fixed() +
-   labs(title='ROC')
+   coord_fixed()
 
 
 
